9076d786bf8cd44c64786f75f5c72bd1c87bbedd
Chris Buckley <62516125+chrisbuckleycode@users.noreply.github.com>
2023-02-06 21:45:57 +0100

    feat: repo backup script

---
+++ backup_github_repos.py
@@ -0,0 +1,42 @@
+import requests
+import json, os
+import shutil
+# json, os are in standard library
+
+backupDir = "/tmp/githubBackup"
+backupZip = "gitbackup.zip"
+# add datestamp in future version
+listRepoNames = []
+repoOwner = "chrisbuckleycode"
+GitHubRepoListUrl = f"https://api.github.com/users/{repoOwner}/repos"
+
+GitHubresponse = requests.get(GitHubRepoListUrl)
+
+json_data = json.loads(GitHubresponse.text)
+
+# get repo names if they have a clone_url
+for repo in GitHubresponse.json():
+    if repo['clone_url']:
+        listRepoNames.append(repo['name'])
+
+# remove temp dir if left over from previously
+try:
+  os.system("rm -rf /tmp/githubBackup")
+except:
+  pass
+
+# create temp dir
+os.system(f"mkdir {backupDir}")
+
+for name in listRepoNames:
+    print(f"\nCloning: https://github.com/{repoOwner}/{name}")
+    os.system(f"git clone https://github.com/{repoOwner}/{name}.git {backupDir}/{name}")
+# add gists in future version
+
+# standard library is messy for zipping, this is cleaner!
+# shutil.make_archive(backupZip, 'zip', backupDir)
+
+# os alternative if not using shutil library
+os.system(f"zip -r {backupZip} {backupDir}")
+
+print(f"\nBackup archive {backupZip} created")
