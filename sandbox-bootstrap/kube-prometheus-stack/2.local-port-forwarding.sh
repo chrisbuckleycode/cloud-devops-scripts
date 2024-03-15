03e2f774ab2ea043dac8fc7fdb5002fb7fb3954d
Chris Buckley <62516125+chrisbuckleycode@users.noreply.github.com>
2024-03-15 23:23:22 +0800

    feat: k8s sandbox helm prom tested successfully

---
+++ sandbox-bootstrap/kube-prometheus-stack/2.local-port-forwarding.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+if [ $# -eq 0 ]; then
+  echo "Please provide the k8s server public IP as an argument."
+  exit 1
+fi
+
+ip_address=$1
+
+echo "When SSH tunneling complete,"
+echo "Visit:"
+echo
+echo "Grafana"
+echo "http://localhost:3000"
+echo "(username: admin"
+echo "password: prom-operator)"
+echo
+echo "Prometheus"
+echo "http://localhost:9090"
+echo
+echo "type \"exit\""
+echo "and Ctrl-C to quit"
+echo "---------------------"
+echo "Enter the password for the remote host"
+
+ssh -L 9090:localhost:9090 -L 3000:localhost:3000 cloud_user@$ip_address
---
+++ sandbox-bootstrap/kube-prometheus-stack/9.remote-kube-prometheus-stack-decomm.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+pkill -f port-forward
+helm uninstall kube-prometheus-stack --namespace monitoring
+kubectl delete crd alertmanagerconfigs.monitoring.coreos.com
+kubectl delete crd alertmanagers.monitoring.coreos.com
+kubectl delete crd podmonitors.monitoring.coreos.com
+kubectl delete crd probes.monitoring.coreos.com
+kubectl delete crd prometheusagents.monitoring.coreos.com
+kubectl delete crd prometheuses.monitoring.coreos.com
+kubectl delete crd prometheusrules.monitoring.coreos.com
+kubectl delete crd scrapeconfigs.monitoring.coreos.com
+kubectl delete crd servicemonitors.monitoring.coreos.com
+kubectl delete crd thanosrulers.monitoring.coreos.com
+kubectl delete ns monitoring
