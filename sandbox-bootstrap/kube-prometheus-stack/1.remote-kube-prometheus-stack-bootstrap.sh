c614659a3562264bdc8d5412c3d478895d8043ce
CB <62516125+chrisbuckleycode@users.noreply.github.com>
2024-03-15 23:41:17 +0800

    fix: clarifying improvements (#19)

--- sandbox-bootstrap/kube-prometheus-stack/1.remote-kube-prometheus-stack-bootstrap.sh
+++ sandbox-bootstrap/kube-prometheus-stack/1.remote-kube-prometheus-stack-bootstrap.sh
@@ -2,13 +2,22 @@
 
 read -s -p "Enter Password for sudo: " sudoPW
 echo $sudoPW | sudo snap install helm --classic
+
 alias k=kubectl
+
+# Install helm chart
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 helm repo update
 helm install kube-prometheus-stack prometheus-community/kube-prometheus-stack --namespace monitoring --create-namespace
+
+# Validate
 kubectl --namespace monitoring get pods -l "release=kube-prometheus-stack" -owide
+
+# Intentional delay
 echo "Waiting 10 seconds for pods to be ready for port-forwarding..."
 sleep 10
 echo "Finished waiting."
+
+# Forward service ports
 kubectl port-forward svc/kube-prometheus-stack-prometheus 9090:9090 -n monitoring &
 kubectl port-forward svc/kube-prometheus-stack-grafana 3000:80 -n monitoring &
